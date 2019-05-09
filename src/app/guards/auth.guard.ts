1f6199e59c1881edea471e6eb6150545bec7de5c
Ashok D <ashokkumar.d@way2online.co.in>
2019-05-09 18:55:04 +0530

    authgaurd updated

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { Observable } from 'rxjs';
-import { resolve } from 'url';
-import { reject } from 'q';
+i
 
 @Injectable({
   providedIn: 'root'
